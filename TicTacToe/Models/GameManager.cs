6119afa02ef4b1e0634ccd7f15030770bcdec54b
TorLindstrom <tor.lindstrom@winlas.se>
2024-09-26 09:04:23 +0200

    refactor: extract a GameManager class
    removing all logic from the program class
    
    --- 45 min done ---

---
+++ TicTacToe/Models/GameManager.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTacToe.Models;
+
+internal abstract class GameManager
+{
+	public static void StartGame()
+	{
+		var output = new Output();
+		var game = new Game();
+		var board = new Board(output, game);
+
+		board.Render();
+	}
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,8 +2,4 @@
 
 using TicTacToe.Models;
 
-var output = new Output();
-var game = new Game();
-var board = new Board(output, game);
-
-board.Render();
+GameManager.StartGame();
